1a316ef20e44da9e1b637ffdbfea7c145bd7aa5e
FrissA <alexis.friss@gmail.com>
2025-03-14 19:25:41 -0300

    created componentsnew

---
+++ src/componentsnew/BulletManager/Bullet.tsx
@@ -0,0 +1,94 @@
+import React, { useRef, useEffect, useState } from "react";
+import { Mesh, Vector3 } from "three";
+import { useFrame, useThree } from "@react-three/fiber";
+
+import { useSound } from "@hooks/useSound";
+import { useGameStore } from "@zustand/store";
+
+import bulletSound from "@assets/sounds/bullet.wav";
+
+interface BulletProps {
+  id: string;
+}
+
+const Bullet: React.FC<BulletProps> = ({ id }) => {
+  const [wasShot, setWasShot] = useState(false);
+  const meshRef = useRef<Mesh>(null);
+  const { pointer } = useThree();
+  const speed = 0.04; // Bullet speed
+  const updateBullet = useGameStore((state) => state.updateBullet);
+  const playerRawPosition = useGameStore((state) => state.playerRawPosition);
+  const bounds = useGameStore((state) => state.bounds);
+
+  const bulletRef = useRef<{ original: Vector3; direction: Vector3 }>({
+    original: new Vector3(),
+    direction: new Vector3(),
+  });
+
+  const bulletAudio = useSound(bulletSound);
+
+  useEffect(() => {
+    // Set the target position for the bullet (where the cursor was when shot)
+    if (meshRef.current && !wasShot) {
+      
+      bulletAudio.play();
+
+      const position = new Vector3(
+        playerRawPosition[0],
+        playerRawPosition[1],
+        playerRawPosition[2]
+      );
+      meshRef.current.position.set(position.x, position.y, bounds.z);
+     
+      bulletRef.current.original = position;
+
+      const target = new Vector3(
+        pointer.x * bounds.maxX,
+        pointer.y * bounds.maxY,
+        bounds.z
+      );
+      bulletRef.current.direction = target;
+
+      setWasShot(true);
+    }
+  }, [bulletAudio, wasShot, playerRawPosition, pointer, updateBullet, id, bounds]);
+
+  // Update bullet position every frame
+  useFrame(() => {
+    if (meshRef.current) {
+      // Move the bullet towards the target
+
+      const direction = new Vector3()
+        .subVectors(bulletRef.current.original, bulletRef.current.direction)
+        .normalize();
+
+      meshRef.current.position.add(direction.multiplyScalar(-speed));
+
+      if (
+        meshRef.current.position.x === 0 &&
+        meshRef.current.position.y === 0
+      ) {
+        meshRef.current.position.set(bounds.maxX, bounds.maxY, bounds.z);
+      }
+
+      const outOfBounds =
+        Math.abs(meshRef.current.position.x) > 10 ||
+        Math.abs(meshRef.current.position.y) > 10;
+
+      updateBullet({
+        id,
+        position: meshRef.current.position,
+        outOfBounds,
+      });
+    }
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <sphereGeometry args={[0.1, 0.1, 0.1]} />
+      <meshStandardMaterial color="blue" />
+    </mesh>
+  );
+};
+
+export default Bullet;
---
+++ src/componentsnew/StartMenu.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+
+import { useGameStore } from "@zustand/store";
+import { GameStatuses } from "@zustand/GameStore";
+import { useSound } from "@hooks/useSound";
+
+import actionSound from '@assets/sounds/action.wav'
+
+const StartScreen: React.FC = () => {
+  const setGameStatus = useGameStore((state) => state.setGameStatus);
+  const actionAudio = useSound(actionSound);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.code === "Space") {
+        actionAudio.play();
+        setTimeout(() => {
+          setGameStatus(GameStatuses.running);
+        },200)
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [setGameStatus, actionAudio]);
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-black text-green-400 font-mono">
+      <div className="border-4 border-green-500 rounded-lg p-6 shadow-lg bg-black/80 backdrop-blur-md max-w-md text-center 
+        animate-terminal border-glow">
+        <h1 className="text-4xl font-[Orbitron] font-bold tracking-wide uppercase animate-fade-in">
+          Welcome to AlepIO
+        </h1>
+        <p className="mt-4 font-[Orbitron] text-lg animate-flicker">
+          Press the <span className="text-white">space bar</span> to start
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default StartScreen;
